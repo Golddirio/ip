3ffea0e8bbafbbd4fdfb57a9de6c86b9301291f3
Golddirio <e1156147@u.nus.edu>
2025-01-24 14:29:44 +0800

    Add mark as done function

--- src/main/java/Darartole.java
+++ src/main/java/Darartole.java
@@ -10,23 +10,45 @@ public class Darartole {
                 + "|____/ \\__,_|_|  \\__,_|_|   \\__\\___/|_|\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("I'm Darartole. What can I do for you?");
-        ArrayList<String> info = new ArrayList<>();
+        ArrayList<Task> info = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String input = scanner.nextLine();
+            Scanner scanInput = new Scanner(input);
+            String firstWord = scanInput.next();
             System.out.println("_____________________________");
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 break;
-            } else if (input.equalsIgnoreCase("list")) {
+            } 
+            if (input.equalsIgnoreCase("list")) {
                 for (int i = 1; i <= info.size(); i++) {
-                    System.out.println(i + ". " + info.get(i - 1));
+                    Task curr = info.get(i-1);
+                    System.out.println(i + ". " + "[" + curr.getStatusIcon() + "] " + curr.getDescription());
                 }
 
+            } else if (scanInput.hasNextInt()) {
+                int taskNo = scanInput.nextInt();
+                
+                if (firstWord.equalsIgnoreCase("mark")) {
+                    Task target = info.get(taskNo - 1);
+                    target.markTask();
+                    System.out.println("Good job. You have just finished one task.");
+                    System.out.println("[" + target.getStatusIcon() + "] " + target.getDescription());
+                
+                } else if (firstWord.equalsIgnoreCase("unmark")) {
+                    Task target = info.get(taskNo - 1);
+                    target.unmarkTask();
+                    System.out.println("I have helped you unmark the task.");
+                    System.out.println("[" + target.getStatusIcon() + "] " + target.getDescription());
+
+                }
+            
             } else {
-                info.add(input);
+                Task task = new Task(input);
+                info.add(task);
                 System.out.println("added: " + input);
             }
             
