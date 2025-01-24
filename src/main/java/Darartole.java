00a2db238e290588d6a36e342f0b79e8e6bd77a8
Golddirio <e1156147@u.nus.edu>
2025-01-24 12:59:28 +0800

    Change the chatbox name

---
+++ src/main/java/Darartole.java
@@ -0,0 +1,13 @@
+public class Darartole {
+    public static void main(String[] args) {
+        String logo = " ____                     _        _      \n"
+                + "|  _ \\  __ _ _ __ __ _ _ __| |_ ___ | | ___ \n"
+                + "| | | |/ _` | '__/ _` | '__| __/ _ \\| |/ _ \\\n"
+                + "| |_| | (_| | | | (_| | |  | || (_) | |  __/\n"
+                + "|____/ \\__,_|_|  \\__,_|_|   \\__\\___/|_|\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("I'm Darartole. What can I do for you?");
+        System.out.println("Bye. Hope to see you again!");
+
+    }
+}
