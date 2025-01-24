7312331a28372919b9112f0db93a5aeaf97f6151
Golddirio <e1156147@u.nus.edu>
2025-01-24 13:44:35 +0800

    Add the list of tasks

--- src/main/java/Darartole.java
+++ src/main/java/Darartole.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Darartole {
     public static void main(String[] args) {
@@ -9,16 +10,26 @@ public class Darartole {
                 + "|____/ \\__,_|_|  \\__,_|_|   \\__\\___/|_|\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("I'm Darartole. What can I do for you?");
+        ArrayList<String> info = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String input = scanner.nextLine();
+            System.out.println("_____________________________");
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 1; i <= info.size(); i++) {
+                    System.out.println(i + ". " + info.get(i - 1));
+                }
+
+            } else {
+                info.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
+            
         }
 
     }
